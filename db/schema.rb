62b3e259da5d4268d3c7da86bd7b52d8981e1987
Carson Bills <CarsonMBills@Gmail.com>
2014-01-05 10:53:05 -0500

    seeds data

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20131212051725) do
   end
 
   create_table "categories", force: true do |t|
-    t.string   "title"
+    t.string   "name"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
